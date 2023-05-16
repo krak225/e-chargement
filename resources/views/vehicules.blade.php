ef57379dae7ae738c75d050d242a9693707c2366
Armand Kouassi <krak225@gmail.com>
2023-05-16 13:10:18 +0000

    update divers

--- resources/views/vehicules.blade.php
+++ resources/views/vehicules.blade.php
@@ -39,7 +39,9 @@
 
 	<div class="wizard-steps clearfix" id="form-wizard"> 
 		<ul class="steps"> 
+			@if($vehicule->vehicule_statuts == "entree2")
 			<li data-target="#step3" class="active"><span class="badge"><i class="fa fa-plus" ></i></span>Nouveau vehicule</li>
+			@endif
 		</ul> 
 	</div> 
 
